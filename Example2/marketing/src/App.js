d581cc84d9d61dbb1578aaa9b18a2739bb2f8eb9
Jack <jackchenyujie.96@gmail.com>
2023-03-08 17:40:53 +0800

    CSS classname de-conflict using generateClassname

--- Example2/marketing/src/App.js
+++ Example2/marketing/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+// Prefix a `ma` in front of the generation of classname e.g.
+// ma1, ma2, ma3
+const generateClassname = createGenerateClassName({
+  productionPrefix: "ma",
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassname}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
