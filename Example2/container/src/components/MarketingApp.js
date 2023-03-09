d2e3c0d0fccf3a61daa90a2c00669fd2133aa569
Jack <jackchenyujie.96@gmail.com>
2023-03-09 16:14:14 +0800

    Section 10: Implementing Multi-Tier Navigation

--- Example2/container/src/components/MarketingApp.js
+++ Example2/container/src/components/MarketingApp.js
@@ -1,12 +1,28 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { mount } from "marketing/MarketingApp";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        // location.pathname is the path we are visiting and
+        // use it to update the history object in container
+        // application
+        // Browser History
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, [history]);
 
   return <div ref={ref} />;
 };
